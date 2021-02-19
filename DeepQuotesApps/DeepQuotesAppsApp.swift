0d673a8f7a31433c3fce23cf2026405acc80ef07
lcs-dsmith <dsmith@lcs.on.ca>
2021-02-19 11:49:30 -0500

    Initial Commit

---
+++ DeepQuotesApps/DeepQuotesAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepQuotesAppsApp.swift
+//  DeepQuotesApps
+//
+//  Created by Dylan Smith on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct DeepQuotesAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
